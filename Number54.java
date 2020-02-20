58d75594dfd539e3997a40d335c7770ef84fa23c
AliHSZ99 <alihsz2002@gmail.com>
2020-02-19 21:19:30 -0500

    Hello

---
+++ Number54.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Number54{
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter the number that you want to the power of 3 > ");
+		double number = scan.nextDouble();
+		
+		System.out.println(number + " to the power of 3 is " +
+							Math.pow(number, 3));
+	}
+}
