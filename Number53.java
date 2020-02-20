eea86c5a41ad684c047f4848e1ffc8886e7c082a
AliHSZ99 <alihsz2002@gmail.com>
2020-02-19 21:19:03 -0500

    Hello

---
+++ Number53.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Number53{
+	public static void main(String[] args){
+
+		Random random = new Random();
+
+		int number1 = random.nextInt(101);
+		int number2 = random.nextInt(101);
+
+		System.out.println("The random numbers are " + number1 +
+							" and " + number2);
+
+		System.out.println("\nThe smallest number is: " + Math.min(number1, number2));
+
+	}
+}
