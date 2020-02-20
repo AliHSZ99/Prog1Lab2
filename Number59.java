b2a3c7e81d6f0ee7a5f00f604da06c02a9762c61
AliHSZ99 <60351680+AliHSZ99@users.noreply.github.com>
2020-02-19 21:27:30 -0500

    Update Number59.java

--- Number59.java
+++ Number59.java
@@ -12,7 +12,7 @@ public class Number59{
 		int number5 = random.nextInt(100 - 60) + 60;
 
 		System.out.println("The five numbers are " + number1 + ", " + number2 +
-							", " + number3 + ", " + number4 + " and " + number5);
+			            ", " + number3 + ", " + number4 + " and " + number5);
 
 		int min1 = Math.min(number1, number2);
 		int min2 = Math.min(number3, number4);
